2014c85d7c0a1c46f62811b85527ac0d95f69124
Nagesh B S <84629129+NageshBS@users.noreply.github.com>
2023-07-29 17:06:12 +0530

    Update Yellow.py

--- Yellow.py
+++ Yellow.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 
 GOOGLE_PLACES_API_KEY = 'AIzaSyD4mBGQhGV_tiV13CXb1HbSnJaG5asgD4I'
-
+# Temporary storage for past 10 locations
 past_locations = []
 
 @app.route('/restaurants', methods=['POST'])
@@ -12,10 +12,12 @@ def get_restaurants():
     data = request.get_json()
     lat = 37.7749
     lon = 122.4194
+    # To Store the current location in past_locations
     past_locations.append({'latitude': lat, 'longitude': lon})
     if len(past_locations) > 10:
         past_locations.pop(0)
 
+    # Get the closest restaurants using the Google Places API
     url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius=5000&type=restaurant&key={AIzaSyD4mBGQhGV_tiV13CXb1HbSnJaG5asgD4I}'
     response = requests.get(url)
     restaurants_data = response.json()
