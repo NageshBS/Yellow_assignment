4ce191f8d8bbd647379aa4cdcb6229e5cfe904ea
Nagesh B S <84629129+NageshBS@users.noreply.github.com>
2023-07-29 16:19:50 +0530

    Add files via upload

---
+++ Yellow.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+GOOGLE_PLACES_API_KEY = 'AIzaSyD4mBGQhGV_tiV13CXb1HbSnJaG5asgD4I'
+
+past_locations = []
+
+@app.route('/restaurants', methods=['POST'])
+def get_restaurants():
+    data = request.get_json()
+    lat = 37.7749
+    lon = 122.4194
+    past_locations.append({'latitude': lat, 'longitude': lon})
+    if len(past_locations) > 10:
+        past_locations.pop(0)
+
+    url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius=5000&type=restaurant&key={AIzaSyD4mBGQhGV_tiV13CXb1HbSnJaG5asgD4I}'
+    response = requests.get(url)
+    restaurants_data = response.json()
+    restaurants = restaurants_data.get('results', [])
+
+    return jsonify({'restaurants': restaurants, 'past_locations': past_locations})
+
+if __name__ == '__main__':
+    app.run(debug=True)
